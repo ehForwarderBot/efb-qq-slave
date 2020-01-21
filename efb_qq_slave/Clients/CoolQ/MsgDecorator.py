84a29ca48522a70e7ee13e0aaec23e6b316d3b3f
milkice233 <milkice@milkice.me>
2020-01-21 22:49:22 +0800

    Patch #19: ?

--- efb_qq_slave/Clients/CoolQ/MsgDecorator.py
+++ efb_qq_slave/Clients/CoolQ/MsgDecorator.py
@@ -181,7 +181,7 @@ class QQMsgProcessor:
         efb_msg.path = efb_msg.file.name
         efb_msg.mime = mime
         efb_msg.filename = data['filename']
-        return [efb_msg]
+        return efb_msg
 
     def qq_group_broadcast_wrapper(self, data, chat: Chat = None):
         try:
